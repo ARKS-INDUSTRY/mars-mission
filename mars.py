8304159c294835d285f134bff3425b680fdcf183
RAKESH Dev <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2022-02-26 15:57:59 +0530

    Update mars.py

--- mars.py
+++ mars.py
@@ -1,4 +1,3 @@
-from stat import filemode
 from time import time
 import urllib.request
 import requests
@@ -8,12 +7,16 @@ from tkinter.messagebox import *
 import os
 import time
 import shutil
+
+
 api = "pIrdFhGHvCOBVwFYeOXvw5PFAxBAUZN2zyCPhPfY"
 base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers"
 root = Tk()
 root.title("Mars Mission")
 root.geometry("550x600")
 root.iconbitmap('icon.ico')
+
+
 def mars_images(rover, camera, sol):
    url = f"{base_url}/{rover}/photos?sol={sol}&camera={camera}&api_key={api}"
    data = requests.get(url)
@@ -24,9 +27,11 @@ def mars_images(rover, camera, sol):
       img_url = data["photos"][i]["img_src"]
       img_url_list.append(img_url)
    return img_url_list
+
 count = 0
 shutil.rmtree('data')
 os.mkdir('data')
+
 def save_img():
    global count
    url_list = listbox.curselection()
@@ -39,8 +44,10 @@ def save_img():
    time.sleep(2)
    img = Image.open(filename)
    img.show()
+
 heading = Label(root, text="Mars Mission! To the mars", font=("helvetica", 30))
 heading.pack(pady=10)
+
 def main_tab():
    global sol, default_rover, default_cam, sol_entry, main_frame, label_rover, cam_selection, sol_label, rover_selection, get_data_btn
    main_frame = Frame(root)
@@ -66,6 +73,7 @@ def main_tab():
    cam_selection.pack(pady=10)
    get_data_btn = Button(text="Get Data",bg="#1EE8A0", width=30, command=result_tab)
    get_data_btn.pack(pady=10)
+
 def get_data():
    try:
       sol = int(sol_entry.get())
@@ -74,6 +82,7 @@ def get_data():
    rover = default_rover.get()
    cam = default_cam.get()
    return rover, cam, sol
+
 def clear_main():
     global rover, sol, cam
     rover, cam, sol = get_data()
@@ -83,30 +92,43 @@ def clear_main():
     sol_label.destroy()
     rover_selection.destroy()
     get_data_btn.destroy()
+
 def clear_result():
+    frame.destroy()
     listbox.destroy()
     go_back_btn.destroy()
     save_btn.destroy()
+    y_scrollbar.destroy()
+
 def result_tab():
    clear_main()
-   global listbox, go_back_btn, save_btn
+   global listbox, go_back_btn, save_btn, y_scrollbar, frame
    data_list = mars_images(rover, cam, sol)
    l = len(data_list)
    if l == 0:
-      showerror("invalid sol", "the entered sol is not proper")
+      showerror("invalid sol", "the entered sol is not proper\nno data available on this sol")
    else:
       pass
-   listbox = Listbox(root, width=55, height=15, font=("aharoni", 10))
+   frame = Frame(root)
+   listbox = Listbox(frame, width=55, height=15, font=("aharoni", 10))
    length = len(data_list)
    for i in range(length):
       listbox.insert(i, data_list[i])
-   save_btn = Button(root, text="Save", bg="#1EE8A0", width=30, command=save_img)
+   y_scrollbar = Scrollbar(frame, orient='vertical')
+   y_scrollbar.pack(side=RIGHT, fill=Y)
+   listbox.config(yscrollcommand=y_scrollbar.set)
+   y_scrollbar.config(command=listbox.yview)
+   x_scrollbar = Scrollbar(frame, orient='horizontal')
+   x_scrollbar.pack(side=BOTTOM, fill=X)
+   listbox.config(xscrollcommand=x_scrollbar.set)
+   x_scrollbar.config(command=listbox.xview)
    listbox.pack(pady=10)
+   frame.pack()
+   save_btn = Button(root, text="Save", bg="#1EE8A0",
+                     width=30, command=save_img)
    save_btn.pack(pady=10)
    go_back_btn = Button(root, text="Go Back", bg="#1EE8A0", width=30, command=main_tab)
    go_back_btn.pack(pady=10)
+
 main_tab()
 root.mainloop()
-
-
-
